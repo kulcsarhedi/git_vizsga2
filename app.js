a9de3d5b42756bf0f737e777ebf84bbe9db460bd
kulcsarhedi <155478571+kulcsarhedi@users.noreply.github.com>
2024-01-13 20:01:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
 
-window.onload = function() {
-    console.log("Az oldal sikeresen betöltődöt");
-};
 
 //Zenék címei
 const songs = ["hey", "summer", "ukulele", "river"];
